7b3a935911e676214f08d009e10fb90f99b2c230
rochelle-pedralba <156513651+rochelle-pedralba@users.noreply.github.com>
2024-07-12 21:06:10 +0800

    Create save_excel.py

---
+++ python/save_excel.py
@@ -0,0 +1,21 @@
+import pandas as pd
+import os
+from openpyxl import load_workbook
+
+def save_excel(rules, country):
+  rules['antecedents'] = rules['antecedents'].apply(lambda x: ', '.join(list(x)))
+  rules['consequents'] = rules['consequents'].apply(lambda x: ', '.join(list(x)))
+
+  excel_path = "../results/association_rules.xlsx"
+
+  if os.path.exists(excel_path):
+    book = load_workbook(excel_path)
+
+    if country in book.sheetnames:
+      del book[country]
+    book.save(excel_path)
+    book.close()
+
+  # Now, write the DataFrame to the Excel file, effectively overwriting the old sheet
+  with pd.ExcelWriter("../results/association_rules.xlsx", engine='openpyxl', mode='a') as writer:
+    rules.to_excel(writer, sheet_name=country, index=False)
